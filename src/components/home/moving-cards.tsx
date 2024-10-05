e4c6b5908583dcad1faeaafe8708551966444d2e
Hmad Afzal <hmadafzal00@gmail.com>
2024-10-05 13:22:48 +0500

    hero section changed

--- src/components/home/moving-cards.tsx
+++ src/components/home/moving-cards.tsx
@@ -6,7 +6,7 @@ import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
 export function MovingCards() {
     return (
         <section className="flex flex-col items-center justify-center px-8 pb-20   relative">
-            <p className="text-lg pb-6 text-primary text-center">We are known by over 60,000+ companies across the world</p>
+            <p className="text-lg pb-6 text-primary text-center">Delivering tailored, world-class solutions to help businesses thrive globally.</p>
                 <InfiniteMovingCards
                     items={qualities}
                     direction="left"
